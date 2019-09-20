7560882eb05f19de9d15c3431b355d7e639967ef
Erik Benton <EBenton@lab.electrostandards.com>
2019-09-20 10:14:23 -0400

    working qsort tested using simple user entered lists/arrays aka N < 100

--- quick_sort.py
+++ quick_sort.py
@@ -1,22 +1,40 @@
-import numpy as np
-
-
 def swap(input_list, index_one, index_two):
     tmp = input_list[index_one]
     input_list[index_one] = input_list[index_two]
     input_list[index_two] = tmp
     return input_list
 
-# Check array size
-    # If array length < 3:
-        # If array length == 2:
-            # if array[0] > array[1]
-                # swap(array, 0, 1)
-        # return array
-# Pick a pivot point
-# Partition the array
-    # Go through the array and compare each value to the pivot point
-        # If it is < pivot, put in array left
-        # If it is >= pivot, put in array right
-    # quick_sort left
-    # quick_sort right
+
+def partition(input_list, left_index, right_index):
+    # Pick a pivot point
+    pivot = input_list[right_index]
+    left = left_index - 1
+    for right in range(left_index, right_index):
+        val = input_list[right]
+        if val <= pivot:
+            left += 1
+            swap(input_list, left, right)
+    # Put pivot in correct index (left + 1) once done
+    swap(input_list, left + 1, right_index)
+    return left + 1
+
+
+def quick_sort(input_list, left_index, right_index):
+    # Check array size
+    if (right_index - left_index) < 2:
+        if (right_index - left_index) == 1:
+            if input_list[left_index] > input_list[right_index]:
+                swap(input_list, right_index, left_index)
+        return input_list
+    # Partition the array and get the pivot's nex index
+    pivot_index = partition(input_list, left_index, right_index)
+    # Sort the left side of the array
+    quick_sort(input_list, left_index, pivot_index - 1)
+    # Sort the right side of the array
+    quick_sort(input_list, pivot_index + 1, right_index)
+    return input_list
+
+
+a = [3, 5, 1, 6, 7, 2, 5, 8, 5, 7]
+quick_sort(a, 0, len(a) - 1)
+print(str(a))
