5840553c3bee2575d7914b3f0e6c51a4538591b3
Erik Benton <EBenton@lab.electrostandards.com>
2019-09-20 08:17:02 -0400

    init commit

---
+++ quick_sort.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+
+# Pick a pivot point
+# Partition the array
+    # Go through the array and compare each value to the pivot point
+        # If it is < pivot, put in array left
+        # If it is >= pivot, put in array right
